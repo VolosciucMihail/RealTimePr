c665f70af653f1c38890f183fe76d1793858a187
VolosciucMihail <56674975+VolosciucMihail@users.noreply.github.com>
2021-08-03 13:18:49 +0300

    Added another worker with supervisor for engagement ration calculation

---
+++ lib/worker_2.ex
@@ -0,0 +1,47 @@
+defmodule Worker2 do
+  use GenServer, restart: :transient
+
+  def start_link(msg) do
+    GenServer.start(__MODULE__, msg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(msg) do
+    {:ok, msg}
+  end
+
+  @impl true
+  def handle_cast({:worker, msg}, _states) do
+    msg_operations(msg)
+    {:noreply, %{}}
+  end
+
+  def msg_operations(msg) do
+    if msg.data =~ "panic" do
+      IO.inspect(%{"Panic message:" => msg})
+      Process.exit(self(), :kill)
+    else
+      json_parse(msg)
+    end
+  end
+
+  def json_parse(msg) do
+      msg_data = Jason.decode!(msg.data)
+      calculate_engagement_ratio(msg_data["message"]["tweet"])
+  end
+
+  def calculate_engagement_ratio(tweet) do
+    favourites_count = tweet["message"]["tweet"]["user"]["favourites_count"];
+    retweet_count = tweet["message"]["tweet"]["retweet_count"];
+    followers_count = tweet["message"]["tweet"]["user"]["followers_count"];
+
+    engagment_ratio = (favourites_count + retweet_count)/followers_count;
+
+    IO.puts("------------------------------------"+engagment_ratio)
+  end
+
+   def get_child(pid) do
+     GenServer.cast(pid, :get)
+   end
+
+end
